887039cf5c782edad735dd8fb438b6c4d827f3d6
Md. Farhan Masud <112246492+masud473@users.noreply.github.com>
2025-10-22 23:31:55 +0600

    Static use

--- Static.cpp
+++ Static.cpp
@@ -5,6 +5,10 @@ class A
     static int a;
 
 public:
+    static void real()
+    {
+        cout << "Something";
+    }
     A()
     {
         a++;
@@ -18,6 +22,7 @@ int A::a = 0;
 int main(int argc, char const *argv[])
 {
     A a1, a2;
+    A::real();
     a1.obj();
     return 0;
 }
