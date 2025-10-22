630911f0aeff0f199094fc8a86b921261be4381c
Md. Farhan Masud <112246492+masud473@users.noreply.github.com>
2025-10-22 11:16:31 +0600

    Added comments to abstraction

--- Abstract.java
+++ Abstract.java
@@ -1,35 +1,51 @@
+
+
+// Abstract class A
 abstract class A {
+    // Concrete method
     void func() {
         System.out.println("Something");
     }
 
+    // Abstract method
     abstract void temp();
 }
 
+// Class B extends A
 class B extends A {
+    // Implementing abstract method
     @Override
     void temp() {
         System.out.println("Something Else");
     }
 }
 
+// Interface C
 interface C {
+    // Abstract method
     void func();
 }
 
+// Class D implements C
 class D implements C {
+    // Implementing interface method
     @Override
     public void func() {
         System.out.println("D implements C");
     }
 }
 
+// Main class
 public class Abstract {
     public static void main(String[] args) {
-        B b=new B();
+        // Create object of B
+        B b = new B();
+        // Call methods
         b.func();
         b.temp();
-        D d =new D();
+        // Create object of D
+        D d = new D();
+        // Call method
         d.func();
     }
 }
